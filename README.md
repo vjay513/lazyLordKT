ed137cebdf3dfae5f2fc9ffeb02a6c670be1ec69
Vijay Bammidi <vijay.bammidi@thomsonreuters.com>
2020-06-20 13:14:51 +0530

    Implement a queue using two stacks

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # lazyLordKT
-Interview-Questions
+Interview Questions
+
+I know we have lot of websites and lot of content in the world, even I am also inspired those websites and documented all questions at one place.
+
+
+
+### Implement a queue using two stacks
+
+A stack is a data structure where items can be added to a collection and removed from it just like in a queue, except the difference is the order in which they are removed. In a stack, the last item added will be the first item removed (last in, first out). Stacks usually have two basic methods, push, which adds an item into the stack, and pop, which "pops off" the top item from the stack. 
+
+Queue is a linear data structure where the first element is inserted from one end called REAR and deleted from the other end called as FRONT. ... In a queue, one end is always used to insert data (enqueue) and the other is used to delete data (dequeue), because queue is open at both its ends.
+
+```Refer question1.js for the solution```
+
+
---
+++ question1.js
@@ -0,0 +1,35 @@
+// Author: lazylord
+var hybridStack = [] // this stack act like queue
+
+Array.prototype.add = function(val) {
+    hybridStack.push(val)
+    // stack1 is array we are using for adding element
+    // for both stack and queue both add elements at the end
+}
+
+Array.prototype.kick = function(){
+    var stack2 = []; // helper stack
+    // here we have to apply our logic
+    // you know will add basic conditions
+    if(this.length === 0){
+        // if the stack1 is empty, we can't remove any thing right
+        return 'There is no elements Lazylord';
+    }else {
+        // we have to apply our brain here
+        // we are queue now, so we have to kick the first one first
+        while(this.length > 0){
+            stack2.push(this.pop())
+        }
+        const ret = stack2.pop(); // popped element
+        
+        while(stack2.length > 0){
+            this.push(stack2.pop())
+        }// After kicking the first element remaining array
+        return ret;
+    }
+}
+
+Array.prototype.remaining = function(){
+    return this;
+}
+
